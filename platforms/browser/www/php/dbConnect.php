8de959a380eebf42686047b5f3e37b1ada61669e
jbk4742 <jbk666@naver.com>
2017-02-06 18:33:50 +0900

    first commit

---
+++ platforms/browser/www/php/dbConnect.php
@@ -0,0 +1,10 @@
+<?php
+    $link = mysqli_connect("localhost","byeongkwan","sjdprp1904!","byeongkwan");
+    mysqli_query($link,"set names utf8");
+     if($link){
+
+        }else{
+                 echo "실패";
+             }
+
+?>
